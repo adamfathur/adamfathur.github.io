1b3dda69c6e43c325029ca251aa19e8128811882
adamfathur <75625756+adamfathur@users.noreply.github.com>
2020-12-28 18:49:10 +0700

    Update script.js

--- learning calculator/script.js
+++ learning calculator/script.js
@@ -1,108 +1,140 @@
 const calculator = {
-    displayValue: '0',
-    firstOperand: null,
-    waitingForSecondOperand: false,
-    operator: null,
-  };
+  displayValue: '0',
+  firstOperand: null,
+  waitingForSecondOperand: false,
+  operator: null,
+};
 
 function inputDigit(digit) {
-    const { displayValue, waitingForSecondOperand } = calculator;
-  
+  const { displayValue, waitingForSecondOperand } = calculator;
+
 if (waitingForSecondOperand === true) {
-      calculator.displayValue = digit;
-      calculator.waitingForSecondOperand = false;
-    } else {
-      calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
-    }
+    calculator.displayValue = digit;
+    calculator.waitingForSecondOperand = false;
+  } else {
+    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
   }
+}
 
 function inputDecimal(dot) {
-    if (calculator.waitingForSecondOperand === true) {
-        calculator.displayValue = "0."
-      calculator.waitingForSecondOperand = false;
-      return
+  if (calculator.waitingForSecondOperand === true) {
+      calculator.displayValue = "0."
+    calculator.waitingForSecondOperand = false;
+    return
+  }
+
+  if (!calculator.displayValue.includes(dot)) {
+    calculator.displayValue += dot;
+  }
+}
+function handleOperator(nextOperator) {
+  const { firstOperand, displayValue, operator } = calculator
+  const inputValue = parseFloat(displayValue);
+  
+  if (operator && calculator.waitingForSecondOperand)  {
+    calculator.operator = nextOperator;
+    return;
+  }
+
+  if (firstOperand == null && !isNaN(inputValue)) {
+      calculator.firstOperand = inputValue;
+    } else if (operator) {
+      const result = calculate(firstOperand, inputValue, operator);
+  
+      calculator.displayValue = `${parseFloat(result.toFixed(7))}`;
+      calculator.firstOperand = result;
     }
   
-    if (!calculator.displayValue.includes(dot)) {
-      calculator.displayValue += dot;
-    }
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = nextOperator;
   }
-function handleOperator(nextOperator) {
-    const { firstOperand, displayValue, operator } = calculator
-    const inputValue = parseFloat(displayValue);
-    
-    if (operator && calculator.waitingForSecondOperand)  {
-      calculator.operator = nextOperator;
-      return;
-    }
-
-    if (firstOperand == null && !isNaN(inputValue)) {
-        calculator.firstOperand = inputValue;
-      } else if (operator) {
-        const result = calculate(firstOperand, inputValue, operator);
-    
-        calculator.displayValue = `${parseFloat(result.toFixed(7))}`;
-        calculator.firstOperand = result;
-      }
-    
-      calculator.waitingForSecondOperand = true;
-      calculator.operator = nextOperator;
-    }
 
 function calculate(firstOperand, secondOperand, operator) {
-        if (operator === '+') {
-          return firstOperand + secondOperand;
-        } else if (operator === '-') {
-          return firstOperand - secondOperand;
-        } else if (operator === '*') {
-          return firstOperand * secondOperand;
-        } else if (operator === '/') {
-          return firstOperand / secondOperand;
-        }
-      
-        return secondOperand;
-      }
-      
+  if (operator === '+') {
+    return firstOperand + secondOperand;
+} else if (operator === '-') {
+    return firstOperand - secondOperand;
+} else if (operator === '*') {
+    return firstOperand * secondOperand;
+} else if (operator === '/') {
+    return firstOperand / secondOperand;
+}
+    return secondOperand;
+}
+    
+//fungsi sin
+function sin(){
+  calculator.displayValue = Math.sin(calculator.displayValue);
+}
+//fungsi cos
+function cos(){
+  calculator.displayValue = Math.cos(calculator.displayValue);
+}
+//fungsi tan
+function tan(){
+  calculator.displayValue = Math.tan(calculator.displayValue);
+}
+//fungsi sqrt
+function sqrt() {
+  calculator.displayValue = Math.sqrt(calculator.displayValue);
+}
+//fungsi kuadrat
+function kuad(){
+  calculator.displayValue = eval(calculator.displayValue) * eval(calculator.displayValue);
+}
+  
 function resetCalculator() {
-        calculator.displayValue = '0';
-        calculator.firstOperand = null;
-        calculator.waitingForSecondOperand = false;
-        calculator.operator = null;
-      }
-      
+      calculator.displayValue = '0';
+      calculator.firstOperand = null;
+      calculator.waitingForSecondOperand = false;
+      calculator.operator = null;
+    }
+    
 function updateDisplay() {
-        const display = document.querySelector('.calculator-screen');
-        display.value = calculator.displayValue;
-      }
-      updateDisplay();
+      const display = document.querySelector('.calculator-screen');
+      display.value = calculator.displayValue;
+    }
+    updateDisplay();
 
 const keys = document.querySelector('.calculator-keys');
-      keys.addEventListener('click', event => {
-        const { target } = event;
-        const { value } = target;
-        if (!target.matches('button')) {
-          return;
-        }
-        
-switch (value) {
-    case '+':
-    case '-':
-    case '*':
-    case '/':
-    case '=':
-      handleOperator(value);
-      break;
-    case '.':
-      inputDecimal(value);
-      break;
-    case 'all-clear':
-      resetCalculator();
-      break;
-    default:
-      if (Number.isInteger(parseFloat(value))) {
-        inputDigit(value);
+    keys.addEventListener('click', event => {
+      const { target } = event;
+      const { value } = target;
+      if (!target.matches('button')) {
+        return;
       }
-  }
 
-  updateDisplay();
+switch (value) {
+  case '+':
+  case '-':
+  case '*':
+  case '/':
+  case '=':
+    handleOperator(value);
+    break;
+    case 'sin':
+    sin(value);
+    break;
+    case 'tan':
+    tan(value);
+    break;
+    case 'cos':
+    cos(value);
+    break;
+    case 'kuad':
+    kuad(value);
+    break;
+  case '.':
+    inputDecimal(value);
+    break;
+  case 'all-clear':
+    resetCalculator();
+    break;
+  default:
+    if (Number.isInteger(parseFloat(value))) {
+      inputDigit(value);
+    }
+}
+
+updateDisplay();
 });
